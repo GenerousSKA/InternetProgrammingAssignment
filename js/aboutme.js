eb732118ac8e09cc63118d82a3b7fc2963945524
GenerousSKA <generousska@gmail.com>
2024-08-21 12:02:25 -0700

    About me html and js changed

---
+++ js/aboutme.js
@@ -0,0 +1,7 @@
+let firstName = "Kareem";
+let lastName = "Shonibare";
+let programName = "Web Development";
+
+document.getElementById("fullName").textContent = firstName + "" + lastName;
+document.getElementById("fullName1").textContent = firstName + "" + lastName;
+document.getElementById("program").textContent = programName;
